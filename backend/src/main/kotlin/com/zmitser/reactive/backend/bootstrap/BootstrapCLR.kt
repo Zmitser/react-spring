d3f23e7359b6e178961fa05641e35c18909cf416
Dmitriy Borisovets <gagat@tut.by>
2017-10-30 21:55:43 +0300

    add command line runner

--- backend/src/main/kotlin/com/zmitser/reactive/backend/bootstrap/BootstrapCLR.kt
+++ backend/src/main/kotlin/com/zmitser/reactive/backend/bootstrap/BootstrapCLR.kt
@@ -4,6 +4,7 @@ import com.zmitser.reactive.backend.domain.Movie
 import com.zmitser.reactive.backend.repositories.MovieRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
 import reactor.core.publisher.toFlux
 import java.util.*
 
@@ -11,27 +12,20 @@ import java.util.*
 class BootstrapCLR constructor(val movieRepository: MovieRepository) : CommandLineRunner {
 
 
-
     override fun run(vararg args: String?) {
 
-
-        //clear old data
-        movieRepository.deleteAll().block()
-
-        listOf("Silence of the Lambdas",
-                "AEon Flux",
-                "Enter the Mono<Void>",
-                "The Fluxxinator",
-                "Back to the Future",
-                "Meet the Fluxes",
-                "Lord of the Fluxes")
-                .toFlux()
-                .map { Movie(UUID.randomUUID().toString(), it) }
-                .flatMap { movieRepository.save(it) }
-                .subscribe(null, null,  { movieRepository.findAll().subscribe{ println(it)}} )
-
-
-
+        movieRepository.deleteAll().thenMany(
+                arrayOf("Silence of the Lambdas",
+                        "AEon Flux",
+                        "Enter the Mono<Void>",
+                        "The Fluxxinator",
+                        "Back to the Future",
+                        "Meet the Fluxes",
+                        "Lord of the Fluxes").toFlux()
+                        .map { Movie(UUID.randomUUID().toString(), it) }
+                .flatMap { movieRepository.save(it) })
+                .subscribe(null, null, { movieRepository.findAll().subscribe { println(it) } }
+                )
     }
 
 }
